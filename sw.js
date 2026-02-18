31fbb63902092b82145a5d797b7e02526688dcdb
segueix <214729927+segueix@users.noreply.github.com>
2026-02-18 18:55:00 +0000

    🔧 Reparació automàtica de categories i cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker per PWA
 
-const CACHE_NAME = 'mytube-v16'; // Canvia el número (v12 -> v13)
+const CACHE_NAME = 'mytube-v17'; // Canvia el número (v12 -> v13)
 const urlsToCache = [
     './',
     './index.html',
